b4e9d44716bcc2bd15093c16493b6396b632326a
Juan Sanchez <jusanchez@unal.edu.co>
2024-01-08 21:12:29 -0500

    Change source.list and some initial configurations added in a different file

---
+++ basicSystem.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -e
+
+sources_dir=/etc/apt/sources.list
+user=$1
+home_user=grep $user /etc/passwd|cut -d: -f 6
+local_bin_dir=$home_user/.local/bin
+backuper_dir=$(pwd)
+
+cp $sources_dir $sources_dir.cp
+cat $backuper_dir/config_files/source.list > $sources_dir
+
+cat <<END
++----------------------------------------------------------------+
+  Testing sources added to $sources_dir.
++----------------------------------------------------------------+
+END
+
+apt-get -y remove --purge "libreoffice*" && apt-get -y clean && apt-get -y autoremove
+
+apt update && apt upgrade && apt full-upgrade && apt autoremove --purge && reboot
