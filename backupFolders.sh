ab363beacded15c05f16d16d2d237e94b05c1a74
Juan Sanchez <jusanchez@unal.edu.co>
2024-01-08 21:15:46 -0500

    Remove unnecessary variables and update folder names

--- backupFolders.sh
+++ backupFolders.sh
@@ -4,19 +4,21 @@ set -e
 
 target=$1
 
-folders=('Term' 'BackUp' 'Boring' 'Calibre Library' 'Code'
-'Desktop' 'Documents' 'Mobile' 'Library' 'Music' 'Obsidian' 'Old'
-'Documentation' 'Pictures' 'Repositories' 'Software' 'Unal' 'Videos', Dotfiles)
+folders=('BackUp' 'Boring' 'Calibre\ Library' 'Code'
+'Desktop' 'Documentation' 'Documents' 'Dotfiles' 'Library' 'Mobile' 'Music' 'Obsidian' 'Old'
+'Pictures' 'Software' 'Term' 'Unal' 'Videos')
 
 # TODO: ensure folder end with /
 
 for folder in "${folders[@]}"; do
     if [[ -d $HOME/$folder ]]; then
-        rsync -a --progress $HOME/$folder $target/backup-$(date '+%Y-%m-%d')/$folder
+        rsync -a --progress $HOME/$folder/ $target/backup-$(date '+%Y-%m-%d')/$folder/
     else 
         echo $key not found.
     fi
 done
+
+rsync -a --progress --exclude $HOME/Repositories/Thunderbird/ $HOME/Repositories/ $target/backup-$(date '+%Y-%m-%d')/Repositories/
 
 cat <<END
 +----------------------------------------------------------------+
--- loadFiles.sh
+++ loadFiles.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
 
-homeFolder=$2
-backu_dir=$3
+backu_dir=$1
 
-folders=('Term' 'BackUp' 'Boring' 'Calibre Library' 'Code'
-'Desktop' 'Documents' 'Mobile' 'Library' 'Music' 'Obsidian' 'Old'
-'Documentation' 'Pictures' 'Repositories' 'Software' 'Unal' 'Videos')
+folders=('BackUp' 'Boring' 'Calibre\ Library' 'Code'
+'Desktop' 'Documentation' 'Documents' 'Dotfiles' 'Library' 'Mobile' 'Music' 'Obsidian' 'Old'
+'Pictures' 'Software' 'Term' 'Unal' 'Videos')
 
 # TODO: ensure folder end with /
 
 
 for folder in "${folders[@]}"; do
     if [[ -d $backu_dir/$folder ]]; then
-        rsync -a --progress $backu_dir/$folder $homeFolder/$folder
+        rsync -a --progress $backu_dir/$folder $HOME/$folder
     else 
         echo $key not found.
     fi
 done
+
+rsync -a --progress --exclude $backu_dir/Repositories/Thunderbird/ $backu_dir/Repositories/ $HOME/Repositories/
+
 
 cat <<END
 +----------------------------------------------------------------+
