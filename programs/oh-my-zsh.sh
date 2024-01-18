8e8a9fef8e780226c830e6e098d2e73e23cf916d
Juan Sanchez <jusanchez@unal.edu.co>
2024-01-18 16:40:04 -0500

    Clone plugin repositories as user

--- programs/oh-my-zsh.sh
+++ programs/oh-my-zsh.sh
@@ -4,12 +4,13 @@ set -e
 
 targetUser=$1
 
-su - $targetUser -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-ln -s /etc/profile.d/vte-2.91.sh /etc/profile.d/vte.sh
-
+su - $targetUser << END
+$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+END
+
+ln -s /etc/profile.d/vte-2.91.sh /etc/profile.d/vte.sh
 
 apt -y install --no-install-recommends fonts-powerline >> /dev/null
 cat <<END
