eb6f0f8323f3714a724ba7a27c64977e205b8b26
Juan Sanchez <jusanchez@unal.edu.co>
2024-01-18 00:31:39 -0500

    Install plugins and fix broken link in tilix

--- programs/oh-my-zsh.sh
+++ programs/oh-my-zsh.sh
@@ -4,8 +4,14 @@ set -e
 
 targetUser=$1
 
-su - $targetUser -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh) && chsh -s $(which zsh)"
+su - $targetUser -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
+ln -s /etc/profile.d/vte-2.91.sh /etc/profile.d/vte.sh
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+apt -y install --no-install-recommends fonts-powerline >> /dev/null
 cat <<END
 +----------------------------------------------------------------+
   Oh my zsh successfully installed.
