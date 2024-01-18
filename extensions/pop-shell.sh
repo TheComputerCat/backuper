603e311aca36e8e6afae825de0ee60311e9f3252
Juan Sanchez <jusanchez@unal.edu.co>
2024-01-18 16:40:27 -0500

    Configure tilix shortcut suing gsettings

--- extensions/pop-shell.sh
+++ extensions/pop-shell.sh
@@ -4,6 +4,11 @@ set -e
 
 extensionsFolder=$HOME/extensions-tmp
 
+gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings "['/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/']"
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ binding "<Shift><Super>Return"
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ command "tilix"
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ name "Tilix"
+
 su -c "apt -y install --no-install-recommends node-typescript >> /dev/null"
 
 mkdir -p $extensionsFolder
