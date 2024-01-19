749de2d4d69662dbb232275e60a7f780edab641c
Juan Sanchez <jusanchez@unal.edu.co>
2024-01-19 00:12:50 -0500

    Unify load and creation of backup

--- backupFolders.sh
+++ backupFolders.sh
@@ -2,25 +2,36 @@
 
 set -e
 
-target=$1
+option=$1
+backu_dir=$2
 
 folders=('BackUp' 'Boring' 'Calibre Library' 'Code'
 'Desktop' 'Documentation' 'Documents' 'Dotfiles' 'Library' 'Mobile' 'Music' 'Obsidian' 'Old'
 'Pictures' 'Software' 'Term' 'Unal' 'Videos')
 
-mkdir -p $target/backup-$(date '+%Y-%m-%d')
+if [[ $option == "-u" ]]; then
+    target=$backu_dir/backup-$(date '+%Y-%m-%d')
+    source=$HOME
+    mkdir -p $target
+elif [[ $option == "-l" ]]; then
+    target=$HOME
+    source=$backu_dir
+else
+    echo Option not recognized.
+    exit 1
+fi
 
 for folder in "${folders[@]}"; do
-    if [[ -d $HOME/"$folder" ]]; then
+    if [[ -d $source/"$folder" ]]; then
         echo Copying "$folder"
-        rsync -au --info=progress2 $HOME/"$folder"/ $target/backup-$(date '+%Y-%m-%d')/"$folder"/
+        rsync -au --info=progress2 $source/"$folder"/ $target/"$folder"/
     else 
         echo "$folder" not found.
     fi
 done
 
-echo Copying $HOME/Repositories and excluding Thunderbird
-rsync -au --info=progress2 --exclude Thunderbird/ $HOME/Repositories/ $target/backup-$(date '+%Y-%m-%d')/Repositories/
+echo Copying $source/Repositories and excluding Thunderbird
+rsync -au --info=progress2 --exclude Thunderbird/ $source/Repositories/ $target/Repositories/
 
 cat <<END
 +----------------------------------------------------------------+
