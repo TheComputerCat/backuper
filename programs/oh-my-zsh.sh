daa7a7ba0efd0b2489ea65a8e1744f251808fee2
Juan Sanchez <jusanchez@unal.edu.co>
2024-01-19 10:33:25 -0500

    Add forgotten path

--- programs/oh-my-zsh.sh
+++ programs/oh-my-zsh.sh
@@ -8,8 +8,8 @@ home_folder=$2
 su - $user -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 su - $user << END
-git clone https://github.com/zsh-users/zsh-autosuggestions $home_folder/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $home_folder/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions $home_folder/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $home_folder/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 END
 
 ln -s /etc/profile.d/vte-2.91.sh /etc/profile.d/vte.sh
