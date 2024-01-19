76b6cc5d576e6708d9a81fb1cebf67f7f3bed1e1
Juan Sanchez <jusanchez@unal.edu.co>
2024-01-19 01:22:29 -0500

    Remove VSCodium .conf

--- backupDotfiles.sh
+++ backupDotfiles.sh
@@ -5,10 +5,9 @@ set -e
 ZSHRC_FILE=~/.zshrc
 GITCONFIG_FILE=~/.gitconfig
 PROFILE_FILE=~/.profile
-CONFIG_FOLDER=~/.config/VSCodium
 OBSIDIAN_FOLDER=~/Obsidian/.obsidian
 DEST=~/Dotfiles/$(date '+%Y-%m-%d')
 
-rsync -au --info=progress2 $ZSHRC_FILE $GITCONFIG_FILE $PROFILE_FILE $CONFIG_FOLDER $OBSIDIAN_FOLDER $DEST
+rsync -au --info=progress2 $ZSHRC_FILE $GITCONFIG_FILE $PROFILE_FILE $OBSIDIAN_FOLDER $DEST
 
 dconf dump / > $DEST/dconfs.ini
